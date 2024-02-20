b5eb0a2d56cc76c89031dc96e2d7ae6fe61f7494
rky <45118204+rkzila123@users.noreply.github.com>
2024-02-20 13:00:40 +0530

    Add files via upload

---
+++ Microservice with OpenFeign and Eureka and API Gateway/With Pre and Post Filter/University-Address-Microservice-Eureka/src/main/java/com/rohit/app/AddressServiceApplication.java
@@ -0,0 +1,21 @@
+package com.rohit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan({"com.rohit.controller", "com.rohit.service"})
+@EntityScan("com.rohit.entity")
+@EnableJpaRepositories("com.rohit.repository")
+@EnableEurekaClient
+public class AddressServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressServiceApplication.class, args);
+	}
+
+}
